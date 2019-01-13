65a22c62faaf12b8e482925ee853ac21a2145d2c
潘晨凯 <pck@panchenkaideMacBook-Pro.local>
2019-01-13 16:27:58 +0800

    1

--- config/index.js
+++ config/index.js
@@ -45,7 +45,8 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '../../',
+        // assetsPublicPath: '../../',
+        assetsPublicPath:'http://webjs.homehawkeye.com/',
         /**
          * Source Maps
          */
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
-
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// Vue.use(Router)
 const Voting = () => import('@/components/voting/voting')
 const Voted = () => import('@/components/voted/voted')
 const Lx = () => import('@/components/lx')
 
-export default new VueRouter({
+export default new myRouter({
     routes: [{
             path: '/voting',
             name: 'voting',
