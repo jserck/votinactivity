64156de26f6322410431ae2ddc15858b128bac4d
潘晨凯 <pck@panchenkaideMacBook-Pro.local>
2019-01-13 15:02:40 +0800

    1

--- config/index.js
+++ config/index.js
@@ -46,7 +46,6 @@ module.exports = {
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '../../',
-
         /**
          * Source Maps
          */
