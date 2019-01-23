7cb546e73b01375cc64755cb9f5f48cd87af0611
潘晨凯 <jscker@sina.com>
2019-01-23 23:21:16 +0800

    1

--- src/assets/js/urlLocation.js
+++ src/assets/js/urlLocation.js
@@ -4,6 +4,6 @@
 
 export const url = {
     baseUrl: 'http://api.cun-tu.cn/activity',
-//     baseUrl: 'http://47.93.193.248:8088/activity',
+    // baseUrl: 'http://47.93.193.248:8088/activity',
     //  baseUrl:'http://192.168.1.240:8080'
 };
