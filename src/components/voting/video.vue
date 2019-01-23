0af40bba30e763b4871d0907651c3e7cfd4b2caa
“roots <wujiajia826@163.com>
2019-01-23 17:12:21 +0800

    Merge branch 'cdn' of github.com:jserck/votinactivity into cdn
    # Conflicts:
    #	config/index.js

--- src/components/voting/video.vue
+++ src/components/voting/video.vue
@@ -1,276 +1,241 @@
 <template>
-    <section class="g-video">
-        <section class="g-video-container">
-            <section class="u-video-book"></section>
-            <section class="u-video-text"></section>
-            <div class="video-play" v-show="isInter">
-              <div class="player">
-                <video-player class="video-player vjs-custom-skin"
-                              ref="videoPlayer"
-                              :playsinline="true"
-                              :options="playerOptions"
-                              @play="onPlayerPlay($event)"
-                              @pause="onPlayerPause($event)"
-                              @ended="onPlayerEnded($event)"
-                >
-                </video-player>
-              </div>
-            </div>
-            <!--<section class="video-play" v-show="isInter">
-                <video-player
-                    class="video-player vjs-custom-skin"
-                    ref="videoPlayer"
-                    :playsinline="true"
-                    :options="playerOptions"
-                    webkit-playsinline="true"
-                    x5-video-player-fullscreen="true"
-                    @play="onPlayerPlay($event)"
-                    @pause="onPlayerPause($event)"
-                    @ended="onPlayerEnded($event)"
-                    @error="errorHandler"
-                    @waiting="onPlayerWaiting($event)"
-                    @statechanged="playerStateChanged($event)"
-                    @canplay="onPlayerCanplaythrough($event)"
-                    @loadeddata="onloadstart($event)"
-                ></video-player>
-            </section>-->
-            <section class="g-inter displayFlex flexColumn flexAlignJustifyCenter" v-if="!isInter">
-                <p>最糟糕的是没网了!</p>
-                <span class="refale" @click="refale">刷新</span>
-            </section>
-            <!-- <section
-                    class="g-inter displayFlex flexColumn flexAlignJustifyCenter"
-                    v-if="isWait&&isInter"
-               >
-                    <p>视频正在努力加载中!</p>
-            </section>-->
-        </section>
+  <section class="g-video">
+    <section class="g-video-container">
+      <section class="u-video-book"></section>
+      <section class="u-video-text"></section>
+      <section class="video-play" v-show="isInter">
+        <video-player
+          class="video-player vjs-custom-skin"
+          ref="videoPlayer"
+          :playsinline="true"
+          :options="playerOptions"
+          webkit-playsinline="true"
+          x5-video-player-fullscreen="true"
+          @play="onPlayerPlay($event)"
+          @pause="onPlayerPause($event)"
+          @ended="onPlayerEnded($event)"
+          @error="errorHandler"
+          @waiting="onPlayerWaiting($event)"
+          @statechanged="playerStateChanged($event)"
+          @canplay="onPlayerCanplaythrough($event)"
+          @loadeddata="onloadstart($event)"
+        ></video-player>
+      </section>
+      <section class="g-inter displayFlex flexColumn flexAlignJustifyCenter" v-if="!isInter">
+        <p>最糟糕的是没网了!</p>
+        <span class="refale" @click="refale">刷新</span>
+      </section>
+      <!-- <section
+              class="g-inter displayFlex flexColumn flexAlignJustifyCenter"
+              v-if="isWait&&isInter"
+         >
+              <p>视频正在努力加载中!</p>
+      </section>-->
     </section>
+  </section>
 </template>
 <script>
-import 'video.js/dist/video-js.css'
-// import 'vue-video-player/src/custom-theme.css'
-import { videoPlayer } from 'vue-video-player';
-export default {
+  import 'video.js/dist/video-js.css'
+  // import 'vue-video-player/src/custom-theme.css'
+  import { videoPlayer } from 'vue-video-player'
+
+  export default {
     props: ['dialogOpations'],
     components: {
-        videoPlayer
+      videoPlayer
     },
     data() {
-        return {
-            isVideo: true,
-            inter: 1,
-            isWait: false,
-            isInter: true,
-            playerOptions: {
-              //  playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
-              autoplay: false, //如果true,浏览器准备好时开始回放。
-              muted: false, // 默认情况下将会消除任何音频。
-              loop: false, // 导致视频一结束就重新开始。
-              preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-              language: 'zh-CN',
-              aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-              fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-              sources: [{
-                type: "",
-                src: this.dialogOpations.src
-              }],
-              poster: "poster.jpg", //你的封面地址
-              width: document.documentElement.clientWidth,
-              notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
-              //  controlBar: {
-              //   timeDivider: true,
-              //   durationDisplay: true,
-              //   remainingTimeDisplay: false,
-              //   fullscreenToggle: true //全屏按钮
-              //  }
-            }
-            /*playerOptions: {
-                playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
-                autoplay: false, //如果true,浏览器准备好时开始回放。
-                muted: false, // 默认情况下将会消除任何音频。
-                loop: false, // 导致视频一结束就重新开始。
-                preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-                language: 'zh-CN',
-                aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-                fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-                sources: [{
-                    type: "",
-                    src: this.dialogOpations.src
-                }],
-                poster: "", //你的封面地址
-                width: document.documentElement.clientWidth,
-                notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
-                controlBar: {
-                    timeDivider: true,//时间分割线
-                    durationDisplay: true,//总时长
-                    //  remainingTimeDisplay: true,
-                    fullscreenToggle: true, //全屏按钮
-                    chaptersButton: false,
-                    playbackRateMenuButton: false,//快速
-                    subtitlesButton: false,
-                    remainingTimeDisplay: true,//倒计时
-                    // progressControl: true,//进度条
-                    volumeMenuButton: {
-                        inline: false,
-                        vertical: true
-                    },//竖着的音量条
-                }
-            }*/
+      return {
+        isVideo: true,
+        inter: 1,
+        isWait: false,
+        isInter: true,
+        playerOptions: {
+          playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+          autoplay: false, //如果true,浏览器准备好时开始回放。
+          muted: false, // 默认情况下将会消除任何音频。
+          loop: false, // 导致视频一结束就重新开始。
+          preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+          language: 'zh-CN',
+          aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+          fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+          sources: [{
+            type: "",
+            src: this.dialogOpations.src
+          }],
+          poster: "", //你的封面地址
+          width: document.documentElement.clientWidth,
+          notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+          controlBar: {
+            timeDivider: true,//时间分割线
+            durationDisplay: true,//总时长
+            //  remainingTimeDisplay: true,
+            fullscreenToggle: true, //全屏按钮
+            chaptersButton: false,
+            playbackRateMenuButton: false,//快速
+            subtitlesButton: false,
+            remainingTimeDisplay: true,//倒计时
+            // progressControl: true,//进度条
+            volumeMenuButton: {
+              inline: false,
+              vertical: true
+            },//竖着的音量条
+          }
         }
+      }
     },
     methods: {
-        hideHandler() {
-            this.$emit('dialogClose', false);
-        },
-        onPlayerPlay() {
+      hideHandler() {
+        this.$emit('dialogClose', false);
+      },
+      onPlayerPlay() {
 
-        },
-        onPlayerPause() {
+      },
+      onPlayerPause() {
 
-        },
-        onPlayerEnded() {
-            this.$refs.videoPlayer.player.src(this.dialogOpations.src);
-        },
-      /*errorHandler() {
-          this.isInter = false;
+      },
+      onPlayerEnded() {
+        this.$refs.videoPlayer.player.src(this.dialogOpations.src);
+      },
+      errorHandler() {
+        this.isInter = false;
       },
       onPlayerWaiting() {
-          if (navigator && navigator.onLine == false) {
-              this.isInter = false;
-              return;
-          }
-          // this.isWait = true;
+        if (navigator && navigator.onLine == false) {
+          this.isInter = false;
+          return;
+        }
+        // this.isWait = true;
       },
       onPlayerCanplaythrough() {
-          // this.isInter = true;
-          // this.isWait = false;
-          if (navigator && navigator.onLine == false) {
-              this.isInter = false;
-          }
+        // this.isInter = true;
+        // this.isWait = false;
+        if (navigator && navigator.onLine == false) {
+          this.isInter = false;
+        }
       },
       playerStateChanged(e) {
-          // if (navigator && navigator.onLine == false) {
-          //      this.isInter = false;
-          // }
+        // if (navigator && navigator.onLine == false) {
+        //      this.isInter = false;
+        // }
       },
       onloadstart(e) {
-          // if (navigator && navigator.onLine == false) {
-          //      this.isInter = false;
-          // }
+        // if (navigator && navigator.onLine == false) {
+        //      this.isInter = false;
+        // }
       },
       refale() {
-          if (!this.isInter) {
-              this.isInter = true;
-          }
-          setTimeout(() => {
-              this.$nextTick(() => {
-                  this.onPlayerEnded()
-              })
-          }, 0)
-      }*/
+        if (!this.isInter) {
+          this.isInter = true;
+        }
+        setTimeout(() => {
+          this.$nextTick(() => {
+            this.onPlayerEnded()
+          })
+        }, 0)
+      }
     },
     computed: {
-        player() {
-            return this.$refs.videoPlayer.player
-        }
+      player() {
+        return this.$refs.videoPlayer.player
+      }
     },
     mounted() {
-        this.$emit("btn_show", 6);
-        if (navigator.userAgent.match(/(iPhone|iPod|iPad);?/i)) {
-            window.addEventListener("offline", function (e) {
-                this.isInter = false;
-            }.bind(this))
-            window.addEventListener("online", function (e) {
-                this.isInter = true;
-            }.bind(this))
-        }
+      this.$emit("btn_show", 6);
+      if (navigator.userAgent.match(/(iPhone|iPod|iPad);?/i)) {
+        window.addEventListener("offline", function (e) {
+          this.isInter = false;
+        }.bind(this))
+        window.addEventListener("online", function (e) {
+          this.isInter = true;
+        }.bind(this))
+      }
     }
-}
+  }
 </script>
 
 <style lang="scss" scoped>
-.g-video {
+  .g-video {
     // overflow: hidden;
     position: relative;
     margin: 30vh auto 0;
     box-sizing: border-box;
     background: #000;
     .g-video-container {
-        .g-inter {
-            width: 100%;
-            line-height: 0.4rem;
-            color: #fff;
-            font-size: 0.2rem;
-        }
+      .g-inter {
+        width: 100%;
+        line-height: 0.4rem;
+        color: #fff;
+        font-size: 0.2rem;
+      }
+      // position: relative;
+      margin: auto;
+      width: 7.03rem;
+      height: 4.21rem /* 490/100 */;
+      @include background("~@/assets/img/videoborder.png");
+      display: flex;
+      transform: none;
+      .video-play {
+        overflow: hidden;
+        margin: auto;
+        width: 6.5rem;
+      }
+      .u-video-book {
+        position: absolute;
+        z-index: 9999999;
+        left: 0.55rem;
+        top: -1.29rem;
+        width: 2.33rem;
+        height: 1.42rem;
+        @include background("~@/assets/img/videobook.png");
+      }
+      .u-video-text {
+        position: absolute;
+        right: 0.68rem;
+        top: -1.46rem;
+        width: 3.98rem;
+        height: 1.56rem;
+        @include background("~@/assets/img/videotit.png");
+      }
+    }
+  }
+  /* 全景（横屏）模式 */
+  @media screen and (orientation: landscape) {
+    .g-video {
+      // overflow: hidden;
+      width: 40%;
+      position: relative;
+      margin: auto;
+      box-sizing: border-box;
+      background: #000;
+      .g-video-container {
         // position: relative;
         margin: auto;
-        width: 7.03rem;
-        height: 4.21rem /* 490/100 */;
-        @include background("~@/assets/img/videoborder.png");
-        display: flex;
-        transform: none;
+        width: 100%;
+        @include background("");
         .video-play {
-            overflow: hidden;
-            margin: auto;
-            width: 6.5rem;
+          overflow: hidden;
+          margin: auto;
+          width: 6.5rem;
         }
         .u-video-book {
-            position: absolute;
-            z-index: 9999999;
-            left: 0.55rem;
-            top: -1.29rem;
-            width: 2.33rem;
-            height: 1.42rem;
-            @include background("~@/assets/img/videobook.png");
+          display: none;
         }
         .u-video-text {
-            position: absolute;
-            right: 0.68rem;
-            top: -1.46rem;
-            width: 3.98rem;
-            height: 1.56rem;
-            @include background("~@/assets/img/videotit.png");
+          display: none;
         }
+      }
     }
-}
-/* 全景（横屏）模式 */
-@media screen and (orientation: landscape) {
-    .g-video {
-        // overflow: hidden;
-        width: 40%;
-        position: relative;
-        margin: auto;
-        box-sizing: border-box;
-        background: #000;
-        .g-video-container {
-            // position: relative;
-            margin: auto;
-            width: 100%;
-            @include background("");
-            .video-play {
-                overflow: hidden;
-                margin: auto;
-                width: 6.5rem;
-            }
-            .u-video-book {
-                display: none;
-            }
-            .u-video-text {
-                display: none;
-            }
-        }
-    }
-}
+  }
 </style>
 <style lang="scss">
-.video-js {
+  .video-js {
     font-size: 0.1rem /* 10/100 */ !important;
-}
-.video-js .vjs-control-bar {
+  }
+  .video-js .vjs-control-bar {
     font-size: 0.14rem /* 14/100 */ !important /* 14/37.5 */;
-}
-.video-js .vjs-big-play-button {
+  }
+  .video-js .vjs-big-play-button {
     width: 0.5rem /* 50/100 */ !important;
     height: 0.5rem /* 50/100 */ !important;
     border-radius: 0.25rem /* 25/100 */ !important ;
@@ -278,8 +243,8 @@ export default {
     left: 50% !important;
     margin-top: -0.25rem /* -25/100 */ !important ;
     margin-left: -0.25rem /* -25/100 */ !important;
-}
-button {
+  }
+  button {
     outline: none !important;
-}
+  }
 </style>
